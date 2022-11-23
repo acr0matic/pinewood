ce148115302383aa7fcee4faa1885665d67130b9
acr0matic <work.acr0matic@gmail.com>
2022-11-23 19:02:34 +0300

    feat: мобильное меню

---
+++ src/scripts/header.js
@@ -0,0 +1,20 @@
+const header = document.getElementById('header');
+
+if (header) {
+  let isReach = false;
+
+  const mobile = header.querySelector('.mobile-menu');
+  const mobileNav = mobile.querySelectorAll('.nav__link');
+  const burger = header.querySelector('.header__burger');
+
+  burger.addEventListener('click', () => Menu());
+  mobileNav.forEach(link => link.addEventListener('click', () => Menu()));
+
+  function Menu() {
+    mobile.classList.toggle('mobile-menu--open');
+    burger.firstElementChild.classList.toggle('is-active')
+    document.body.classList.toggle('disable-scroll');
+  }
+}
+
+
